43fdbcd415188ea79e46d6618474179d626fa29c
Juan Diego <juandsilva028@gmail.com>
2026-05-13 20:39:00 -0500

    add new limit

--- Persistence/EntityConfigurations/ProductConfigurations/ProductReadConfiguration.cs
+++ Persistence/EntityConfigurations/ProductConfigurations/ProductReadConfiguration.cs
@@ -13,7 +13,7 @@ public class ProductReadConfiguration: IEntityTypeConfiguration<ProductReadModel
         
         builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
         builder.Property(p => p.Description).IsRequired().HasMaxLength(500);
-        builder.Property(p => p.PictureUrl).IsRequired().HasMaxLength(500);
+        builder.Property(p => p.PictureUrl).IsRequired().HasMaxLength(1000);
         builder.Property(p => p.Type).IsRequired().HasMaxLength(100);
         builder.Property(p => p.Brand).IsRequired().HasMaxLength(100);
         builder.Property(p => p.PublicId).HasMaxLength(100);
