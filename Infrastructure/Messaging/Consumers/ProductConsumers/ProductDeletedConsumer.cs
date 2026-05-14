3fec8a651ecda31b9af722e63618effe4ba7b09c
Juan Diego <juandsilva028@gmail.com>
2026-05-13 20:58:19 -0500

    delete custom product

--- Infrastructure/Messaging/Consumers/ProductConsumers/ProductDeletedConsumer.cs
+++ Infrastructure/Messaging/Consumers/ProductConsumers/ProductDeletedConsumer.cs
@@ -13,10 +13,13 @@ public class ProductDeletedConsumer(ReadDbContext readContext, ILogger<ProductDe
     {
         var readModel = await readContext.Products
             .FirstOrDefaultAsync(p => p.Id == context.Message.ProductId, context.CancellationToken);
+        var readCustomModel = await readContext.CustomProducts
+            .FirstOrDefaultAsync(p => p.Id == context.Message.ProductId, context.CancellationToken);
 
-        if (readModel != null)
+        if (readModel != null && readCustomModel != null)
         {
             readContext.Products.Remove(readModel);
+            readContext.CustomProducts.Remove(readCustomModel);
             await readContext.SaveChangesAsync(context.CancellationToken);
             logger.LogInformation("Product with id {ProductId} has been deleted", context.Message.ProductId);
         }
