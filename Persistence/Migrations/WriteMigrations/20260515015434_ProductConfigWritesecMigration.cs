058b956940e6b0cea94223c5a1690b8ade9b9fb7
Juan Diego <juandsilva028@gmail.com>
2026-05-14 20:55:36 -0500

    add

---
+++ Persistence/Migrations/WriteMigrations/20260515015434_ProductConfigWritesecMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations.WriteMigrations
+{
+    /// <inheritdoc />
+    public partial class ProductConfigWritesecMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
